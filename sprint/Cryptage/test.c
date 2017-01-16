6c9ba3b050c49a2ba0a7cea23fcb8574ef5c5cca
ejalaa12 <ejalaa12@gmail.com>
2017-01-16 17:06:38 +0100

    now compatible with armadeus

---
+++ sprint/Cryptage/test.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void encrypt(char * message, int degree) {
+
+	int n = 0, rotateSwap = 0;
+	int i;
+
+	for(i=0; i<strlen(message); i++){
+		if(message[i] >='a' && message[i] <='z'){
+			n = 'z' - message[i];
+			if( degree > n ){
+				rotateSwap = degree - n - 1;
+				message[i] = 'a';
+				message[i] += rotateSwap;
+			}  
+			else{
+				message[i] = message[i] + degree;
+			}
+		}
+	}
+}
+
+int main()
+{
+	char * message = (char *) malloc(sizeof(char)*1);
+	int  degree;
+	printf("Enter message:");
+	scanf("%s", message);
+	printf("Enter code:");
+	scanf("%d", &degree);
+
+
+  // printf("Enter String: ");
+  // gets(message);
+  // printf("Enter Number: ");
+  // scanf("%i", &degree);
+
+	encrypt(message, degree);
+
+	int j;
+	for(j = 0; message[j] !='\0'; j++){
+		printf("%c", message[j]);
+	}
+
+	printf("\n");
+
+	return 0;
+}
